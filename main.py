25b283ca10ffab30b330b396240ed8e7e8821950
Kevin Tobar <3710557250101@ingenieria.usac.edu.gt>
2024-06-05 22:41:24 -0600

    Tkinter Login
    implementación de login provisional con tkinter para usarse y modificarse ademas solo cree la ventana de admin y un boton para regresar nada mas

--- main.py
+++ main.py
@@ -1,12 +1,67 @@
-print('Hola mundo')
+import tkinter as tk
+from tkinter import messagebox
+class applicacion:
+    def __init__(self, root) :
+        self.root=root
+        self.root.title("IPC2 MARKET")
+        self.root.geometry("300x200")
+    
+        self.create_widgets()
+    
+    def create_widgets(self):
+        # Crear etiquetas y entradas
 
-print('Se crea branch 202010367')
+        self.label_tittle=tk.Label(self.root,text="IPC2 MARKET",font=("Roboto Cn",14))
+        self.label_tittle.pack(pady=5)
+        self.label_user = tk.Label(self.root, text="Usuario:")
+        self.label_user.pack(pady=5)
+        
+        self.entry_user = tk.Entry(self.root)
+        self.entry_user.pack(pady=5)
+        
+        self.label_password = tk.Label(self.root, text="Contraseña:")
+        self.label_password.pack(pady=5)
+        
+        self.entry_password = tk.Entry(self.root, show="*")
+        self.entry_password.pack(pady=5)
+        
+        # Botón de login
+        self.button_login = tk.Button(self.root, text="Login",command=self.verifUsuarios)
+        self.button_login.pack(pady=10)
+    
+    def verifUsuarios(self):
+        #obtener valores de los textbox
+        user=self.entry_user.get()
+        password=self.entry_password.get()
+        #aqui va la logica de inicio de sesion entre admin y usuarios por el momento solo 
+        #verificare el admin
+        if user=="AdminIPC2" and password=="IPC2VJ2024":
+            messagebox.showinfo(title="Exito", message="Bienvenido Administrador")
+            self.crear_ventanaAdmin()
+  
+        else:
+            messagebox.showerror(title="error",message="DATOS INCORRECTOS")
 
+    
+    def crear_ventanaAdmin(self):
+        self.root.withdraw()
+        self.top = tk.Toplevel(self.root)
+        self.top.title("Crear Estudiante")
+        self.button_back=tk.Button(self.top,text="Cerrar Sesion",command=self.regresarLogin)
+        self.button_back.pack(pady=10)
 
+    def regresarLogin(self):
+        # Cerrar la ventana principal
+        self.top.destroy()
+        self.entry_user.delete(0, tk.END)
+        self.entry_password.delete(0, tk.END)
+        
+        # Mostrar la ventana de login
+        self.root.deiconify()
 
-print('Iniciando prueba branch 202010367')
-
-
-print('Iniciando prueba branch 202200236')
-
-#holaaa
+if __name__=="__main__":
+    root=tk.Tk()
+    app=applicacion(root)
+    root.mainloop()
+    
+    
