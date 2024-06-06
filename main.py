ade4b29948428db309d5b9b760f6f88cfbb5d97d
MarceJua <mjuarez2017ig@gmail.com>
2024-06-05 19:23:40 -0600

    Se crea branch 202200236

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 print('Hola mundo')
-<<<<<<< HEAD
+
 print('Se crea branch 202010367')
 
-=======
+
 
 print('Iniciando prueba branch 202010367')
->>>>>>> branch_202010367
+
+
+print('Iniciando prueba branch 202200236')
