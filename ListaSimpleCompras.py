8e417abea79064e9fe35f797e92a60f0103089bc
Kevin Tobar <3710557250101@ingenieria.usac.edu.gt>
2024-06-10 22:22:05 -0600

    Creacion de nodo LIsta Simple

--- ListaSimpleCompras.py
+++ ListaSimpleCompras.py
@@ -1,3 +1,8 @@
 class  NodoCompras:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,idUsuarios,nombreUsuario,NombreProductos,total) -> None:
+        self.idUsuarios=idUsuarios
+        self.nombreUsuario=nombreUsuario
+        self.NombreProductos=NombreProductos
+        self.total=total
+        self.siguiente=None
+        
