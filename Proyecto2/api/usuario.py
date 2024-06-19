3a07de2f55b5b7f0f88f6871276e2e41f6f8e8d1
MarceJua <mjuarez2017ig@gmail.com>
2024-06-18 21:37:12 -0600

    Se agrega carpeta Proyecto2 y se agrega funcionalidad de lector de XML usuarios en API

---
+++ Proyecto2/api/usuario.py
@@ -0,0 +1,11 @@
+class Usuario():
+    def __init__(self, id, password, nombre, edad, email, telefono):
+        self.id = id
+        self.password = password
+        self.nombre = nombre
+        self.edad = edad
+        self.email = email
+        self.telefono = telefono
+
+    def getUsuario(self):
+        return self
