eebaa6276a0b2f4180f59563f4b77ddf9c425751
Kevin Tobar <3710557250101@ingenieria.usac.edu.gt>
2024-06-20 00:23:41 -0600

    Metodos post y get de productos y valiaciones (aun falta lo de id)
    falta id unico mañana lo hago

--- Proyecto2/api/main.py
+++ Proyecto2/api/main.py
@@ -31,5 +31,28 @@ def get_usuarios():
     c = manager.get_usuario()
     return jsonify(c), 200
 
+@app.route('/cargaproductos',methods=['POST'])
+def addProductos():
+    xml=request.data.decode('utf-8')
+    raiz=ET.XML(xml)
+    for elemento in raiz:
+        id=elemento.attrib["id"]
+        nombre=elemento.find('nombre').text
+        precio=elemento.find('precio').text
+        descripcion=elemento.find('descripcion').text
+        categoria=elemento.find('categoria').text
+        cantidad=elemento.find('cantidad').text
+        imagen=elemento.find('imagen').text
+        manager.add_producto(id,precio, nombre,descripcion,categoria,cantidad,imagen)
+    return jsonify({'msg': 'Archivo XML cargado satisfactoriamente'}), 200
+
+@app.route('/showallproduct', methods=['GET'])
+def get_productos():
+    c = manager.get_productos()
+    return jsonify(c), 200
+
+
+
 if __name__=='__main__':
     app.run(debug=True, port=8000) #debug=True <- sirve para no estar reiniciando la api
+
